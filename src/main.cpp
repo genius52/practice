f3d2b5d7abb302bd92d5106e2828fb2df7b73bee
genius52 <genius52@qq.com>
2024-12-17 10:11:47 +0800

    upgrade to c++17

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,18 @@
 #include <bitset>
 #include <stack>
 #include <queue>
+#include <list>
+#include <shared_mutex>
 //#include <bits/stdc++.h>
 using namespace std;
 
 
 int main() {
+    std::shared_lock< rw_mtx;
+
+    std::vector<int>::const_iterator it;
+    vector<string> v;
+    deque<string> q(v.begin(),v.end());
 
     return 0;
 }
